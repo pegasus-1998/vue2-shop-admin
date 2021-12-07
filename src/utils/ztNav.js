b97c383ea962ad0d126c0410a11a1775fb14bc9a
pgs <2226984343@qq.com>
2021-12-08 00:52:45 +0800

    jian

--- src/utils/ztNav.js
+++ src/utils/ztNav.js
@@ -1,17 +1,17 @@
 const ztNavList = [
     {
-        icon: 'el-icon-edit',
+        icon: 'el-icon-orange',
         kinds: [
             {
-                text: '轮播效果',
+                text: '双向轮播',
                 path: '/bazaar/swiper',
             },
             {
-                text: '内衣',
+                text: '轮播',
                 path: '/404',
             },
             {
-                text: '玩具',
+                text: '轮播',
                 path: '/404'
             }
         ]
@@ -20,11 +20,11 @@ const ztNavList = [
         icon: 'el-icon-share',
         kinds: [
             {
-                text: '男子',
+                text: '常见动画效果',
                 path: '/404'
             },
             {
-                text: '裤子',
+                text: '交互',
                 path: '/404'
             }
         ]
