27da03d5d81f09d2265ad36621d72dec26a7f7d6
pgs <2226984343@qq.com>
2022-01-18 00:06:54 +0800

    add imgs

--- src/mock/message/shopMes.js
+++ src/mock/message/shopMes.js
@@ -1,7 +1,7 @@
 export default [
     {
         "id": "0",
-        "src": "https://img.alicdn.com/imgextra/i1/112097249/O1CN01NY97E523Q5m4ag0ax_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop1.webp",
         "con": "奢姿2021夏季新款肥mm宽松大码女装V领减龄显瘦星星印花连衣裙",
         "price": "255",
         "nowPrice": "129",
@@ -10,7 +10,7 @@ export default [
     },
     {
         "id": "1",
-        "src": "https://img.alicdn.com/imgextra/i4/44242389/O1CN010hS2JP1TWCtZWcK6A_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop2.webp",
         "con": "百洛安短袖圆领连衣裙女夏季新款宽松显瘦垂坠百褶裙长款裙子",
         "price": "598",
         "nowPrice": "346",
@@ -19,7 +19,7 @@ export default [
     },
     {
         "id": "2",
-        "src": "https://img.alicdn.com/imgextra/i4/29855095/O1CN01pXW5dY1nVYdmCn45q_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop3.webp",
         "con": "高端大码女装胖mm2021年夏季新款碎花连衣裙遮肉显瘦漂亮时尚裙子",
         "price": "598",
         "nowPrice": "598",
@@ -28,7 +28,7 @@ export default [
     },
     {
         "id": "3",
-        "src": "https://img.alicdn.com/imgextra/i4/32883064/O1CN01b1PFJG1YVMEWefdI1_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop4.webp",
         "con": "颜域碎花百褶衬衫连衣裙夏装2021新款通勤大摆优雅气质收腰中长裙",
         "price": "732",
         "nowPrice": "439",
@@ -37,7 +37,7 @@ export default [
     },
     {
         "id": "4",
-        "src": "https://img.alicdn.com/imgextra/i4/44242389/O1CN01gnmR9D1TWCsDK83MS_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop5.webp",
         "con": "百洛安圆领无袖连衣裙夏季新款时尚简约通勤高腰显瘦百褶裙长裙子",
         "price": "698",
         "nowPrice": "296",
@@ -46,7 +46,7 @@ export default [
     },
     {
         "id": "5",
-        "src": "https://img.alicdn.com/imgextra/i2/1861120180/O1CN01VPXtuW1DCTyYQA5gR_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop6.webp",
         "con": "连衣裙女2021夏季新款甜美气质格子法式小个子收腰西装领裙子女装",
         "price": "169",
         "nowPrice": "98",
@@ -55,7 +55,7 @@ export default [
     },
     {
         "id": "6",
-        "src": "https://img.alicdn.com/imgextra/i2/28752524/O1CN015xHRa41UW2MiD4E3L_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop7.webp",
         "con": "魅斑小心机橘粉色短袖连衣裙2021年夏季新款气质V领中腰修身A字裙",
         "price": "825",
         "nowPrice": "825",
@@ -64,7 +64,7 @@ export default [
     },
     {
         "id": "7",
-        "src": "https://img.alicdn.com/imgextra/i3/30147688/O1CN01Q4S7Ee26f9ZIq1jy9_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop8.webp",
         "con": "烟花烫2021连衣裙秋季新品女装七分袖气质淑女网纱印花裙SD 丝音",
         "price": "338",
         "nowPrice": "338",
@@ -73,7 +73,7 @@ export default [
     },
     {
         "id": "8",
-        "src": "https://img.alicdn.com/imgextra/i4/30301515/O1CN01UwtFA21N3ujBbGlIS_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop9.webp",
         "con": "Annally2021夏季新款优雅气质通勤修身显瘦大摆深蓝色雪纺连衣裙",
         "price": "444",
         "nowPrice": "444",
@@ -82,7 +82,7 @@ export default [
     },
     {
         "id": "9",
-        "src": "https://img.alicdn.com/imgextra/i1/14430479/O1CN01amVWfd1FPQOS5pU9V_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop10.webp",
         "con": "戎美【QZ0316289】敲奢重磅双层真丝 21春夏新款鬼绉立领连衣裙",
         "price": "4996",
         "nowPrice": "780",
@@ -91,7 +91,7 @@ export default [
     },
     {
         "id": "10",
-        "src": "https://img.alicdn.com/imgextra/i1/29587467/O1CN01ff4VZ8251w1PX8WWZ_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop11.webp",
         "con": "时尚a字印花裙子女2021新款夏装欧美修身气质中长款雪纺连衣裙女",
         "price": "670",
         "nowPrice": "289",
@@ -100,7 +100,7 @@ export default [
     },
     {
         "id": "11",
-        "src": "https://img.alicdn.com/imgextra/i4/12968373/O1CN01oAwAJC2BisqOhBrC9_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop12.webp",
         "con": "欧美大牌2021春夏新款重磅真丝连衣裙高端桑蚕丝女装粉色长裙直身",
         "price": "559",
         "nowPrice": "339",
@@ -109,7 +109,7 @@ export default [
     },
     {
         "id": "12",
-        "src": "https://img.alicdn.com/imgextra/i3/14430479/O1CN01Bg3od21FPQNM19w4j_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop13.webp",
         "con": "戎美【QZ0405751】震撼航海线描/依约印花/手工描绘打褶半身裙",
         "price": "13996",
         "nowPrice": "499",
@@ -118,7 +118,7 @@ export default [
     },
     {
         "id": "13",
-        "src": "https://img.alicdn.com/imgextra/i4/34688650/O1CN01VtfHE02DlkdAL5Kcb_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop14.webp",
         "con": "FUSSED原创小众设计师款品牌雾霾灰蓝蕾丝边别致设计感连衣裙女夏",
         "price": "520",
         "nowPrice": "520",
@@ -127,7 +127,7 @@ export default [
     },
     {
         "id": "14",
-        "src": "https://img.alicdn.com/imgextra/i2/26183357/O1CN01OoZjDn1afYChs8GWJ_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop15.webp",
         "con": "素木碎花雪纺连衣裙2021新款夏欧洲站女装宽松中长款a字裙子潮货",
         "price": "159",
         "nowPrice": "159",
@@ -136,7 +136,7 @@ export default [
     },
     {
         "id": "15",
-        "src": "https://img.alicdn.com/imgextra/i3/56015919/O1CN01AZSzYi1tax03lXsuS_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop16.webp",
         "con": "真丝连衣裙夏季2021新款时尚收腰高端法式碎花裙奢华大牌桑蚕丝裙",
         "price": "1888",
         "nowPrice": "639",
@@ -145,7 +145,7 @@ export default [
     },
     {
         "id": "16",
-        "src": "https://img.alicdn.com/imgextra/i3/32960648/O1CN0100xspS1GepJeyYgcF_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop17.webp",
         "con": "王小鸭杭州真丝连衣裙女2021夏季新款弹力缎面法式复古印花妈妈裙",
         "price": "1199",
         "nowPrice": "799",
@@ -154,7 +154,7 @@ export default [
     },
     {
         "id": "17",
-        "src": "https://img.alicdn.com/imgextra/i4/30301515/O1CN013ZR0mP1N3ui8YSYES_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop18.webp",
         "con": "Annally2021夏装新款气质优雅修身显瘦淑女A字粉紫色蕾丝连衣裙女",
         "price": "459",
         "nowPrice": "298",
@@ -163,7 +163,7 @@ export default [
     },
     {
         "id": "18",
-        "src": "https://img.alicdn.com/imgextra/i3/1480540058/O1CN01y6LEIS1CIbeeOlIu7_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop19.webp",
         "con": "青青张 2021夏季新款法式初恋甜美公主风荷叶边白色收腰连衣裙女",
         "price": "148",
         "nowPrice": "148",
@@ -172,7 +172,7 @@ export default [
     },
     {
         "id": "19",
-        "src": "https://img.alicdn.com/imgextra/i4/56015919/O1CN011R9MzW1tax0DrQSF8_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop20.webp",
         "con": "凯芙诺真丝连衣裙夏2021新款收腰显瘦法式碎花裙时尚减龄洋气裙子",
         "price": "1888",
         "nowPrice": "686",
@@ -181,7 +181,7 @@ export default [
     },
     {
         "id": "20",
-        "src": "https://img.alicdn.com/imgextra/i4/96937318/O1CN01o7QZGX23vh6RrbT7I_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop21.webp",
         "con": "恒源祥雪纺裙妈妈夏装洋气高贵短袖2021新款显瘦中老年女装连衣裙",
         "price": "298",
         "nowPrice": "199",
@@ -190,7 +190,7 @@ export default [
     },
     {
         "id": "21",
-        "src": "https://img.alicdn.com/imgextra/i1/32960648/O1CN01aI3aYY1GepFX4NggH_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop22.webp",
         "con": "王小鸭V领大码杭州真丝连衣裙女2021新款夏港风收腰宽松中长A字裙",
         "price": "1268",
         "nowPrice": "868",
@@ -199,7 +199,7 @@ export default [
     },
     {
         "id": "22",
-        "src": "https://img.alicdn.com/imgextra/i4/13908605/O1CN01u7Pehg2DR8pzC9gJG_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop23.webp",
         "con": "复古学院风百褶连衣裙2021夏季新款设计感蝴蝶结饰小个子英伦裙子",
         "price": "208",
         "nowPrice": "208",
@@ -208,7 +208,7 @@ export default [
     },
     {
         "id": "23",
-        "src": "https://img.alicdn.com/imgextra/i1/30313259/O1CN01Zfo2LJ1ZwfTDqEKzY_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop24.webp",
         "con": "【店长推荐】JESSIE粉色减龄碎花V领真丝连衣裙21夏新款DKSCL605",
         "price": "2399",
         "nowPrice": "1389",
@@ -217,7 +217,7 @@ export default [
     },
     {
         "id": "24",
-        "src": "https://img.alicdn.com/imgextra/i4/18327577/O1CN01TPioaW25qJgGoeKJV_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop25.webp",
         "con": "ADA MISS夏2021新款中长款气质法式超仙黑色短袖收腰直筒连衣裙女",
         "price": "319",
         "nowPrice": "319",
@@ -226,7 +226,7 @@ export default [
     },
     {
         "id": "25",
-        "src": "https://img.alicdn.com/imgextra/i3/30147688/O1CN01hzc6D126f9cCaw545_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop26.webp",
         "con": "烟花烫2021秋季新款文艺清新连衣裙女仙气大摆雪纺网纱裙SD 青汁",
         "price": "368",
         "nowPrice": "368",
@@ -235,7 +235,7 @@ export default [
     },
     {
         "id": "26",
-        "src": "https://img.alicdn.com/imgextra/i2/521580123/O1CN01L4neEB1CmNOB7wi5F_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop27.webp",
         "con": "素木女时尚V领雪纺裙子2021夏季新品欧洲站五分袖系带碎花连衣裙",
         "price": "289",
         "nowPrice": "199",
@@ -244,7 +244,7 @@ export default [
     },
     {
         "id": "27",
-        "src": "https://img.alicdn.com/imgextra/i3/128366668/O1CN01DExs4o1z7zesunXsH_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop28.webp",
         "con": "初海 碎花连衣裙女藏蓝色泡泡袖夏季v领复古茶歇裙短款桔梗裙子",
         "price": "256",
         "nowPrice": "128",
@@ -253,7 +253,7 @@ export default [
     },
     {
         "id": "28",
-        "src": "https://img.alicdn.com/imgextra/i3/29151216/O1CN01i4kAmL1KqyJMFkpaZ_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop29.webp",
         "con": "迪赛尼斯裙子2021夏季新款高腰超仙小清新天丝白色短袖连衣裙女夏",
         "price": "799",
         "nowPrice": "469",
@@ -262,7 +262,7 @@ export default [
     },
     {
         "id": "29",
-        "src": "https://img.alicdn.com/imgextra/i1/24306848/O1CN01Bi5FOD20SQwsQNPWJ_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop30.webp",
         "con": "qtu女装亚麻圆领短袖中长连衣裙2021新款夏显瘦别致小黑裙LQ2896",
         "price": "810",
         "nowPrice": "649",
@@ -271,7 +271,7 @@ export default [
     },
     {
         "id": "30",
-        "src": "https://img.alicdn.com/imgextra/i4/15171914/O1CN01Jypv9y1Q0ejy2zDoB_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop31.webp",
         "con": "柒柒家 2021秋冬新款侧开叉V领针织连衣裙女法式小众绑带收腰长裙",
         "price": "299",
         "nowPrice": "299",
@@ -280,7 +280,7 @@ export default [
     },
     {
         "id": "31",
-        "src": "https://img.alicdn.com/imgextra/i4/10622858/O1CN01VmHb1D1Wz0d18aUjS_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop32.webp",
         "con": "水墨青华2021夏季新款修身显瘦蕾丝鱼尾裙优雅气质连短袖连衣裙",
         "price": "520",
         "nowPrice": "338",
@@ -289,7 +289,7 @@ export default [
     },
     {
         "id": "32",
-        "src": "https://img.alicdn.com/imgextra/i2/32883064/O1CN0105pQcE1YVMEQo6lsr_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop33.webp",
         "con": "颜域假两件V领短袖连衣裙女夏2021年新款气质收腰显瘦中长款裙子",
         "price": "655",
         "nowPrice": "399",
@@ -298,7 +298,7 @@ export default [
     },
     {
         "id": "33",
-        "src": "https://img.alicdn.com/imgextra/i1/127617910/O1CN01Kupyte28IpZUTFMLJ_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop34.webp",
         "con": "OVV2021春夏提花面料V领长袖法式连衣裙GQLCJ11003A",
         "price": "899",
         "nowPrice": "899",
@@ -307,7 +307,7 @@ export default [
     },
     {
         "id": "34",
-        "src": "https://img.alicdn.com/imgextra/i1/15748037/O1CN01Fx5bXD29EzrsiN2on_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop35.webp",
         "con": "红色连衣裙女2021夏季新款方领法式复古气质修身长裙泡泡袖订婚裙",
         "price": "588",
         "nowPrice": "178",
@@ -316,7 +316,7 @@ export default [
     },
     {
         "id": "35",
-        "src": "https://img.alicdn.com/imgextra/i2/15705899/O1CN01RQacyJ1tRn4nJ9UZ6_!!0-saturn_solar.jpg_468x468q75.jpg_.webp",
+        "src": "https://raw.githubusercontent.com/wiki/pegasus-1998/images/shop36.webp",
         "con": "Damostudio21夏新 法式经典浪漫V领围裹式黄色碎花收腰连衣裙",
         "price": "238",
         "nowPrice": "238",
